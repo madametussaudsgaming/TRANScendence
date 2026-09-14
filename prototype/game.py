c9b5d9a705d5d23d9def92822938ab75fce45fe6
FurizAlex <Alexanderchin714@gmail.com>
2026-09-14 12:37:21 +0800

    Log [GAME]: Wrote some data in board.json

---
+++ prototype/game.py
@@ -0,0 +1,18 @@
+# -------------------------------
+# CORE GAMEPLAY
+# -------------------------------
+
+def offerPurchase():
+	pass
+
+def calculateRent():
+	pass
+
+def transferMoney(player, rent):
+	pass
+
+def sendToJail():
+	pass
+
+def auction():
+	pass
