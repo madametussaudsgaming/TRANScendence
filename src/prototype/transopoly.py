bfef8e846544bdebdffa4226cd5293b6caff25d7
FurizAlex <Alexanderchin714@gmail.com>
2026-09-14 13:02:05 +0800

    Log [GAME]: Seperated functions between more files --tile.py --property.py --trading.py

---
+++ src/prototype/transopoly.py
@@ -0,0 +1,39 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    transopoly.py                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alechin <alechin@student.42kl.edu.my>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/09/14 12:53:39 by alechin           #+#    #+#              #
+#    Updated: 2026/09/14 12:53:39 by alechin          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# ---------------------------
+# CORE GAME LOGIC
+# ---------------------------
+
+def newGame():
+	pass
+
+def rollNMove():
+	pass
+
+def sendToJail():
+	pass
+
+def buildHouse(playerId, position):
+	pass
+
+def endTurn(playerId):
+	pass
+
+def getState(playerId):
+	pass
+
+def main():
+	print("Hello World!")
+
+if __name__ == "__main__":
+	main()
